951b93b625a4d54b65d27d9e288e2b65c90a2899
Giuliano Machado <giufma@gmail.com>
2022-01-23 20:34:41 -0300

    add push and pop program

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const IP = 0;
 const ACC = 1;
 const R1 = 2;
 const R2 = 3;
+const R3 = 4;
+const R4 = 5;
+const R5 = 6
+const R6 = 7;
+const R7 = 8;
+const R8 = 9;
+const SP = 10;
+const FP = 11;
 
 const memory = createMemory(256 * 256);
 // create writable Uint8 array
@@ -17,34 +25,45 @@ const cpu = new CPU(memory);
 
 let i = 0; // incrementing pointer
 
-writableBytes[i++] = instructions.MOV_MEM_REG;
-writableBytes[i++] = 0x01;
-writableBytes[i++] = 0x00; // 0x0100
-wirtableBytes[i++] = R1;
+///////////// PROGRAM /////////////
+//
+// mov $5151, R1
+// mov $4242, R2
+//
+// psh R1
+// psh R2
+//
+// pop R1
+// pop R2
+//
 
 writableBytes[i++] = instructions.MOV_LIT_REG;
-writableBytes[i++] = 0x00;
-writableBytes[i++] = 0x01; // 0x0001
-wirtableBytes[i++] = R2;
-
-writableBytes[i++] = instructions.ADD_REG_REG;
+writableBytes[i++] = 0x51;
+writableBytes[i++] = 0x51;
 writableBytes[i++] = R1;
-wirtableBytes[i++] = R2;
 
-writableBytes[i++] = instructions.MOV_REG_MEM;
-writableBytes[i++] = ACC;
-writableBytes[i++] = 0x01;
-wirtableBytes[i++] = 0x00; // 0x0100
+writableBytes[i++] = instructions.MOV_LIT_REG;
+writableBytes[i++] = 0x42;
+writableBytes[i++] = 0x42;
+writableBytes[i++] = R2;
 
-writableBytes[i++] = instructions.JMP_NOT_EQ;
-writableBytes[i++] = 0x00;
-writableBytes[i++] = 0x03; // 0x0001
-wirtableBytes[i++] = 0x00;
-writableBytes[i++] = 0x00;
+writableBytes[i++] = instructions.PSH_REG;
+writableBytes[i++] = R1;
+
+writableBytes[i++] = instructions.PSH_REG;
+writableBytes[i++] = R2;
+
+writableBytes[i++] = instructions.POP;
+writableBytes[i++] = R1;
+
+writableBytes[i++] = instructions.POP;
+writableBytes[i++] = R2;
+
+///////// END OF PROGRAM //////////
 
 cpu.debug();
 cpu.viewMemoryAt(cpu.getRegister('ip'));
-cpu.viewMemoryAt(0x0100);
+cpu.viewMemoryAt(0xffff - 1 - 6);
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -55,5 +74,5 @@ rl.on('line', () => {
   cpu.step();
   cpu.debug();
   cpu.viewMemoryAt(cpu.getRegister('ip'));
-  cpu.viewMemoryAt(0x0100);
+  cpu.viewMemoryAt(0xffff - 1 - 6);
 });
